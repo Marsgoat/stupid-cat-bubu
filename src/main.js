d8ecfc46a9a997f0bf7ba535089beffe9b4275af
Marsgoat <johnny3681@gmail.com>
2022-03-23 22:10:06 +0800

    zach cat test

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    frame: false, // 不顯示標題列
+    transparent: true, // 背景透明
+    autoHideMenuBar: true, // 不顯示工具列
+  });
+
+  win.loadFile('./src/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
