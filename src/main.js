474d3941f37d77f1ab04afc342b076541bcf599e
Marsgoat <johnny3681@gmail.com>
2022-03-24 14:36:01 +0800

    setAlwaysOnTop

--- src/main.js
+++ src/main.js
@@ -3,16 +3,19 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 250,
+    height: 250,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
     frame: false, // 不顯示標題列
     transparent: true, // 背景透明
     autoHideMenuBar: true, // 不顯示工具列
+    resizable: false, // 不可改變視窗大小
+    center: true, // 目前視窗置中
   });
 
+  win.setAlwaysOnTop(true, 'screen-saver'); // 畫面置頂
   win.loadFile('./src/index.html');
 }
 
