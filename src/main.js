7bd53e7a1b4b1c44c47c822dd6fffa54a2ee4586
Marsgoat <johnny3681@gmail.com>
2022-03-26 23:59:42 +0800

    createTray

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const { Menu, Tray } = require('electron');
 const path = require('path');
 
 function createWindow() {
@@ -17,6 +18,7 @@ function createWindow() {
 
   win.setAlwaysOnTop(true, 'screen-saver'); // 畫面置頂
   win.loadFile('./src/index.html');
+  createTray(win);
 }
 
 app.whenReady().then(() => {
@@ -30,3 +32,23 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
+
+function createTray(win) {
+  const iconPath = path.join(__dirname, '../img/icon.jpg');
+  const tray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '最小化',
+      click: () => win.hide(),
+    },
+    {
+      label: '結束',
+      click: () => app.quit(),
+    },
+  ]);
+  tray.setToolTip('stupid cat');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => win.show());
+  return tray;
+}
